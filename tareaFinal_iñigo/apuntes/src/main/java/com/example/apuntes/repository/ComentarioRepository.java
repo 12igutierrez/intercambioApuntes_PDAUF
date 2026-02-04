e7d1fb4809324b459c09116827586da692c98368
Íñigo Gutiérrez <12igutierrez75@gmail.com>
2026-02-04 14:07:16 +0100

    Delete tareaFinal_iñigo directory

--- tareaFinal_iñigo/apuntes/src/main/java/com/example/apuntes/repository/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.apuntes.repository;
-
-import com.example.apuntes.model.Comentario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
-    List<Comentario> findByApunteId(Long apunteId);
-}
--- tareaFinal_iñigo/apuntes/src/main/java/com/example/apuntes/repository/MensajeContactoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.apuntes.repository;
-
-import com.example.apuntes.model.MensajeContacto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MensajeContactoRepository
-        extends JpaRepository<MensajeContacto, Long> {
-}
